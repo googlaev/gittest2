7a4dbb5a9519be540c091ae07fc3bb58bcde4071
Your Name <you@example.com>
2021-06-08 08:40:26 +0500

    первый комит запись

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+           
+
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                a = 5 / a;
+                Console.WriteLine(a);
+            }catch(DivideByZeroException)
+            {
+                Console.WriteLine("error деление на 0");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("ввод букв вместо цифр");
+            }
+            
+        }   
+    }
+}
